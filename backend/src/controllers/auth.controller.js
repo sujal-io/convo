6de7a9eb5eb15bb0e4b049edd9a5433af4d5753c
Sujal Bhardwaj <bhardwaj0601@gmail.com>
2026-01-24 17:17:12 +0530

    Login and Logout controllers added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -65,3 +65,40 @@ export const signup = async (req, res) => {
         return res.status(500).json({message: 'Internal server error'});
     }
 }
+
+export const login = async(req,res) =>{
+  const {email,password} = req.body;
+
+  if(!email || !password){
+    return res.status(400).json({message: 'All fields are required'});
+  }
+
+  try{
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(404).json({message: 'Invalid credentials'});
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if(!isPasswordCorrect){
+      return res.status(404).json({message: 'Invalid credentials'});
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+       _id: user._id,
+       fullname: user.fullname,
+       email: user.email,
+       profilePic: user.profilePic
+    })
+  } catch (error){
+    console.error("Login error:", error);
+    return res.status(500).json({message: 'Internal server error'});
+  }
+}
+
+export const logout = (_,res) =>{
+   res.cookie("jwt", "", {maxAge:0});
+   res.status(200).json({message: 'Logout successful'});
+}
