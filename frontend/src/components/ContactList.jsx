c54b22620b25fd6c94186a9738e58d83d53ec72f
Sujal Bhardwaj <bhardwaj0601@gmail.com>
2026-02-08 16:46:01 +0530

    Sidebar completed

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -27,7 +27,7 @@ function ContactList() {
                 <img src={contact.profilePic || "/avatar.png"} />
               </div>
             </div>
-            <h4 className="text-slate-200 font-medium">{contact.fullName}</h4>
+            <h4 className="text-slate-200 font-medium">{contact.fullname}</h4>
           </div>
         </div>
       ))}
