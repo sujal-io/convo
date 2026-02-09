94b39de920f8db752f651e127d10c1b139081ce7
Sujal Bhardwaj <bhardwaj0601@gmail.com>
2026-02-09 20:57:55 +0530

    Chat Header component's functionality implemented

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import { useAuthStore } from "../store/useAuthStore";
+import ChatHeader from "./ChatHeader";
 
 function ChatContainer() {
+  const { selectedUser, getMessagesByUserId, messages } = useChatStore();
+  const { authUser } = useAuthStore();  
+
+  useEffect(() => {
+   getMessagesByUserId(selectedUser._id);
+  }, [selectedUser, getMessagesByUserId]);
+
   return (
-    <div>ChatContainer</div>
+    <>
+    <ChatHeader />
+    </>
   )
 }
 
