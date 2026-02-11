8a22a3f795cbae6dc2278cb626f6840bc733519c
Sujal Bhardwaj <bhardwaj0601@gmail.com>
2026-02-11 23:36:08 +0530

    Implement chat message UI with conditional alignment

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -17,7 +17,33 @@ function ChatContainer() {
     <ChatHeader />
     <div className="flex-1 px-6 overflow-y-auto  py-8">
       {messages.length >0 ?(
-        <p>Messages</p>
+       <div className="max-w-3xl mx-auto space-y-6">
+            {messages.map((msg) => (
+              <div
+                key={msg._id}
+                className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+              >
+                <div
+                  className={`chat-bubble relative ${
+                    msg.senderId === authUser._id
+                      ? "bg-cyan-600 text-white"
+                      : "bg-slate-800 text-slate-200"
+                  }`}
+                >
+                  {msg.image && (
+                    <img src={msg.image} alt="Shared" className="rounded-lg h-48 object-cover" />
+                  )}
+                  {msg.text && <p className="mt-2">{msg.text}</p>}
+                  <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
+                    {new Date(msg.createdAt).toLocaleTimeString(undefined, {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
       ) : (
         <NoChatHistoryPlaceholder name = {selectedUser.fullname} />
       )}
