b3044ff0145c3141d11e26dfdc98e09b33bafecb
Sujal Bhardwaj <bhardwaj0601@gmail.com>
2026-02-23 22:50:27 +0530

    feat: implement createGroup controller

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import groupRoutes from './routes/group.route.js';
 import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
 import cookieParser from 'cookie-parser';
@@ -16,7 +17,8 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/messages',messageRoutes)
+app.use('/api/messages',messageRoutes);
+app.use('/api/groups', groupRoutes);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
